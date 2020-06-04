099d875b8f6f36305ae3d40121c8da16af4e8a59
samy <samybn@yahoo.com>
2020-06-04 18:26:20 +0200

    L'avantage de cette méthode c'est qu'elle est utilisable meme avec les fichiers lourd

--- copy.js
+++ copy.js
@@ -1,15 +1,29 @@
-let fs = require ('fs');
+let fs = require('fs');
 
 let file = "touchpiano.wav"
 
 let read = fs.createReadStream(file)
 
-read.on('data', (chunk)=> {
-  console.log("J'ai lu " + chunk.length);
-  
-})
+fs.stat(file, (err ,stats) => {
 
-read.on('end', () => {
-  console.log("J'ai fini de lire le fichier");
+  let total = stats.size
+   
+  let progress = 0 ;
+
+ 
+
+  read.on('data', (chunk) => {
+    
+    
+     progress += chunk.length 
+
+     console.log("J'ai lu " + Math.round(100 * progress / total ) + "%" );
+     
+  })
   
+  read.on('end', () => {
+    console.log("J'ai fini de lire le fichier");
+    
+  })
+
 })
